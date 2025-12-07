be7c927874f304cf20fab56ae1808d59f42b4d44
heeisboy <70956397+heeisboy@users.noreply.github.com>
2025-12-07 12:05:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-API_TOKEN = '5396556060:AAFBluMwOZ-rd6LnDkfiUNLj55yLhSN7vCM' # сюда токен бота (взять тут - t.me/BotFather)
-admin = 5293987235 # id админа (взять тут - t.me/getmyid_bot)
+API_TOKEN = '' # сюда токен бота (взять тут - t.me/BotFather)
+
+admin = ### # id админа (взять тут - t.me/getmyid_bot)
